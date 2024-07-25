d2f71ca2702d6a954620eb8fe72feb91b6f4f553
muhamad-hammad <m.hammad.bhatti09@gmail.com>
2024-07-25 21:54:32 +0500

    intro page almost completed

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 
@@ -8,41 +10,67 @@ class Intropage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      // ignore: prefer_const_constructors
       body: Center(
-        child: const Column(
-          children: [
-            //logo
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Image(
-                image: AssetImage('lib/images/logo.jpg'),
-                height: 240,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Image(
+                  image: AssetImage('lib/images/logo.png'),
+                  height: 240,
+                ),
               ),
-            ),
-        
-            SizedBox(height: 29),
-        
-            //title
-            Text('Mountain Dew',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                )),
-        
-            SizedBox(height: 10),
-        
-            //title
-            Text('Dar k agai jeet ha',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.black,
-                ))
-        
-            //button
-          ],
+          
+              const SizedBox(height: 29),
+          
+              //title
+              const Text('Mountain Dew',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
+                  ),
+          
+              const SizedBox(height: 10),
+          
+              //title
+              const Text('Dar k agai jeet ha ',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black,
+                  )),
+          
+              //button
+              const SizedBox(height: 10),
+              GestureDetector(
+                // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const Homepage())),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  padding: const EdgeInsets.all(25),
+                  child: const Center(
+                    child: Text(
+                      'kharid lai',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
