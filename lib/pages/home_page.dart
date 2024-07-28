f832b82053faef3d78eb7dddec4e79100a6b52ea
muhamad-hammad <m.hammad.bhatti09@gmail.com>
2024-07-28 09:16:06 +0500

    updated drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -36,10 +36,100 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[400],
-      body: _pages[_selectedIndex],
       bottomNavigationBar: MyBottomNavBar(
         onTabChange: (index) => navigateBottomBar(index),
       ),
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: Builder(
+            builder: (context) {
+              return IconButton(
+                icon: const Padding(
+                  padding: EdgeInsets.only(left: 12.0),
+                  child:  Icon(
+                    Icons.menu,
+                    color: Colors.black,
+                  ),
+                ),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              );
+            }
+          )
+        ),
+        drawer: Drawer(
+          backgroundColor: Colors.grey[900],
+          child : Column(
+            mainAxisAlignment : MainAxisAlignment.spaceBetween,
+              children: [
+              //logo
+              Column(
+
+                children: [
+                  DrawerHeader(
+                    child: Image.asset(
+                      'lib/images/logo.png',
+                      color: Colors.white
+                    ),
+                  ),
+                              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Divider(color: Colors.grey[888],),
+                              ),
+                              //other pages
+                              const Padding(
+                  padding: EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.home,
+                      color: Colors.white,
+                      ),
+                    title: Text(
+                      'Home',
+                      style: TextStyle(color: Colors.white),
+                      
+                      )
+                  ),
+                              ),
+                              const Padding(
+                  padding: EdgeInsets.only(left: 25.0),
+                  child: const ListTile(
+                    leading: Icon(
+                      Icons.info,
+                      color: Colors.white,
+                      ),
+                    title: Text(
+                      'About',
+                      style: TextStyle(color: Colors.white),
+                      
+                      )
+                  ),
+                              ),
+                ],
+              ),
+            const Padding(
+              padding: EdgeInsets.only(left: 25.0, bottom: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                  ),
+                title: Text(
+                  'Logout',
+                  style: TextStyle(color: Colors.white),
+                  
+                  )
+              ),
+            )
+
+
+            ],
+
+          )
+        ),
+      body: _pages[_selectedIndex],
     );
   }
 }
