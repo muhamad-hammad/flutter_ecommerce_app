189ab7c2170f302d7023b9dc49f92436df466eda
muhamad-hammad <m.hammad.bhatti09@gmail.com>
2024-07-30 17:15:02 +0500

    made everything alligned and good looking

--- lib/Components/shoe_tile.dart
+++ lib/Components/shoe_tile.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import '../models/shoe.dart';
 
@@ -9,87 +11,95 @@ class ShoeTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.only(left: 25.0),
-        width: 200,
-        decoration: BoxDecoration(
-          color: Colors.grey[300],
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Column(
-          children: [
-            //Shoe picture
-            ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Image.asset(shoe.imagePath),
+      margin: const EdgeInsets.all(15.0),
+      width: 200,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Shoe picture
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.asset(
+              shoe.imagePath,
+              height: 150,
+              width: double.infinity,
+              fit: BoxFit.cover,
             ),
+          ),
           const SizedBox(height: 10),
-            //Description
-            Text(shoe.description,
-            textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.grey[600],
-                )),
-
-            //price + details
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+          // Description
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              shoe.description,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.grey[600],
+                fontSize: 14,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          // Price + details
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          //shoe name
-                          Text(
-                            shoe.name,
-                            style: TextStyle(
-                              color: Colors.grey[600],
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          //little space gap
-                          const SizedBox(height: 5),
-                  
-                          //price
-                          Text(
-                            '\$${shoe.price}',
-                            style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                        ],
-                      )
-                    ],
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Shoe name
+                    Text(
+                      shoe.name,
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    // Price
+                    Text(
+                      '\$${shoe.price}',
+                      style: TextStyle(
+                        color: Colors.green[700],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                // Button to add to cart
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
                   ),
                 ),
-                
-                //button to add to cart
-                Container(
-                  alignment: Alignment.bottomRight,
-                  padding: const EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        bottomRight: Radius.circular(12),
-                      )),
-                  //Add to cart icon:
-                  child:
-                  const Icon(
-                    Icons.add, 
-                    color: Colors.white,
-                    ),
-                ),
               ],
-            )
-          ],
-        ));
+            ),
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
   }
 }
